2f559c0ceca330dc6005d5438c9309db1f7d5f53
mshimada <mshimada2@mail.csuchico.edu>
2018-01-31 17:22:30 -0800

    fixed spelling

--- madlibs.cpp
+++ madlibs.cpp
@@ -21,7 +21,7 @@ cin>>order;
 cout<<"Enter a number with a decimal: "<<endl;
 cin>>price;
 
-cout<<"One day, Kober decided to go get food since he was hungry. "<<endl;
+cout<<"One day, Kobe decided to go get food since he was hungry. "<<endl;
 cout<<"He decided to go to "<<name<<". "<<endl;
 cout<<"Once there, he ordered number "<<order<<". "<<endl;
 cout<<"It was quite the deal since it only cost "<<price<<"! "<<endl;
