00b1090b08e6c5c3dc199122153d2abaa60bfc56
mshimada <mshimada2@mail.csuchico.edu>
2018-01-31 17:20:50 -0800

    Final version

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,35 @@
-//Authors: 
+//Authors: McDermott Liam Shimada 
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+
+string name;
+int order;
+double price;
+
 int main()
 {
+
+cout<<"Enter a noun: "<<endl;
+cin>>name;
+
+cout<<"Enter a number: "<<endl;
+cin>>order;
+
+cout<<"Enter a number with a decimal: "<<endl;
+cin>>price;
+
+cout<<"One day, Kober decided to go get food since he was hungry. "<<endl;
+cout<<"He decided to go to "<<name<<". "<<endl;
+cout<<"Once there, he ordered number "<<order<<". "<<endl;
+cout<<"It was quite the deal since it only cost "<<price<<"! "<<endl;
+
+
+
+
+
 
   return 0;
 }
